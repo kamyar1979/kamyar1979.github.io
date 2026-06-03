83e29010d875c82ad26ca0fc9348733ec4a2dc7b
Kamyar Inanloo <kamyar1979@gmail.com>
2026-06-03 09:38:36 +0330

    Initial portfolio

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Kamyar Inanloo Portfolio
+
+npm install
+npm run dev
+
+Publish through GitHub Pages.
---
+++ astro.config.mjs
@@ -0,0 +1,5 @@
+
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://kamyar1979.github.io'
+});
